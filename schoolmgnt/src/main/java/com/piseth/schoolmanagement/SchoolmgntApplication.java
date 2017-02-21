f284f1f2b559dde8f32a2be3e76cf3087a756a79
piseth <chpiseth09@gmail.com>
2017-02-20 23:30:08 -0600

    first commit

---
+++ schoolmgnt/src/main/java/com/piseth/schoolmanagement/SchoolmgntApplication.java
@@ -0,0 +1,12 @@
+package com.piseth.schoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmgntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmgntApplication.class, args);
+	}
+}
---
+++ schoolmgnt/src/test/java/com/piseth/schoolmanagement/SchoolmgntApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piseth.schoolmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolmgntApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
